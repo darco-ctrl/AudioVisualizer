64f9a67583c94aa7c0fcfb4d68c7317055212055
darco-ctrl <darcowb388@gmail.com>
2026-04-08 22:02:27 +0530

    added FFT process

--- scripts/globals/SampleData.cs
+++ scripts/globals/SampleData.cs
@@ -2,23 +2,37 @@ using Godot;
 using System;
 using NAudio.Wave;
 using System.Linq;
+using System.Collections;
+using System.Numerics;
+
+using NWindow = MathNet.Numerics.Window;
+using NFourier = MathNet.Numerics.IntegralTransforms.Fourier;
+using MathNet.Numerics.IntegralTransforms;
 
 public partial class SampleData : Node
 {
 
-	public int RingBuffer = 1024;
-
-	private int WriteIndex = 0;
+	public const int RING_BUFFER = 1024;
+	public const int FFT_SIZE = 2048;
 
 	private readonly object LockObject = new object();
 
-	public float[] Samples;
+	private float[] samples;
+	private float[] frame;
+
+	readonly double[] _window = NWindow.Hann(FFT_SIZE);
 
 	private WasapiLoopbackCapture Capture;
 
+	private int writeIndex = 0;
+	private int framePos = 0;
+
+	private bool queuedFFT = false;
+
 	public override void _Ready()
 	{
-		Samples = new float[RingBuffer];
+		samples = new float[RING_BUFFER];
+		frame = new float[FFT_SIZE];
 
 		Capture = new WasapiLoopbackCapture();
 		Capture.DataAvailable += Capture_DataAvailable;
@@ -34,24 +48,59 @@ public partial class SampleData : Node
 			{
 				float sample = BitConverter.ToSingle(_eventArgs.Buffer, i * 4);
 
-				Samples[WriteIndex] = sample;
+				samples[writeIndex] = sample;
 
-				WriteIndex = (WriteIndex + 1) % RingBuffer;
+				writeIndex = (writeIndex + 1) % RING_BUFFER;
+
+				frame[framePos++] = sample;
+
+				if (framePos >= FFT_SIZE)
+				{
+					queuedFFT = true;
+					framePos = 0;
+				}
+			}
+
+			if (queuedFFT)
+			{
+				FFTProcess();
 			}
 		}
-	}	
+	}
+
+	
+
+	private void FFTProcess()
+	{
+		queuedFFT = false;
+
+		double[] window = _window;
+		var fftBuffer = new Complex[FFT_SIZE];
+
+		for (int i = 0; i < FFT_SIZE; i++)
+		{
+			frame[i] = frame[i] * (float)window[i];
+
+			fftBuffer[i] = new Complex(frame[i], 0);
+
+		}
+
+		NFourier.Forward(fftBuffer, FourierOptions.Matlab);
+		
+	}
 
 	public float[] GetSamples()
 	{
 		lock (LockObject)
 		{
-			return Samples.ToArray();
+			return samples.ToArray();
 		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 
     public override void _ExitTree()
